0d59430e443152173af19c2f9e8af5a1fe8670d3
lovepreet <lovep1730@gmail.com>
2024-04-17 19:12:55 +0530

    init

---
+++ internal/handlers/api.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/LovepreetSinghMe/goapi/internal/middleware"
+	"github.com/go-chi/chi"
+	chimiddle "github.com/go-chi/chi/middleware"
+)
+
+func Handler(r *chi.Mux) {
+	// Global middleware
+	r.Use(chimiddle.StripSlashes)
+
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+		jsonByte, _ := json.Marshal(map[string]string{"msg": "Not Found"})
+
+		w.Write([]byte(jsonByte))
+	})
+
+	r.Route("/account", func(router chi.Router) {
+		router.Use(middleware.Authorization)
+		router.Get("/coins", GetCoinBalance)
+	})
+}
